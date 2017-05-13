ee481e4fd1c68d0dbdbe9e92531d1788cb35f916
Shane C Mason <shane.c.mason@gmail.com>
2017-05-12 23:18:15 -0700

    Change line endings...

--- setup.py
+++ setup.py
@@ -22,9 +22,9 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.6.0',
+    version='0.6.5',
 
-    description='EssentialDB - NOSQL document database.',
+    description='EssentialDB - pure Python document database.',
     long_description=long_description,
 
     # The project's main homepage.
